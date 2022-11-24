d5fcdabf40bbde9e0c874f14178108f66ea6054c
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2022-11-24 18:23:11 -0300

    refactor: GET request on route /users/:email

--- src/middlewares/emailSchemaValidation.middleware.js
+++ src/middlewares/emailSchemaValidation.middleware.js
@@ -1,12 +1,13 @@
 import { emailSchema } from "../schemas/email.schema.js";
 
 export function emailSchemaValidation(req, res, next) {
-  const email = req.body;
+  const { email } = req.params;
 
-  const { error } = emailSchema.validate(email, { abortEarly: false });
+  const { error } = emailSchema.validate({ email }, { abortEarly: false });
 
   if (error) {
     const erros = error.details.map((detail) => detail.message);
+    console.log(erros);
     return res.status(400).send(erros);
   }
 
