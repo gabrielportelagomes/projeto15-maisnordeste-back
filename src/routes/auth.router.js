6c4f4213084735ba3c11cdc8e734963ab8259afb
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2022-11-24 14:27:43 -0300

    feat: POST request on route /sign-in

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { authValidation } from "../middlewares/authValidation.middleware.js";
+import { postSignIn } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
+
+authRouter.post("/sign-in", authValidation, postSignIn);
 
 export default authRouter;
